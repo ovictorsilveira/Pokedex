c5f015e51ac4b2912cf385632d92bff322f6ed0c
ovictorsilveira <ovictorsilveira@icloud.com>
2022-03-01 15:34:15 -0300

    integração da lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    HttpClientModule
 
   ],
   providers: [],
